05eb3a9f9669be6587b798af528be1cc6e601034
Adesh Dhanraj Bode <adesh_dhanrajbode@epam.com>
2026-03-06 13:46:03 +0530

    Singleton Setup & Enum impl + ENV loaded

---
+++ src/main/java/org/designpattern/singleton/Enum.java
@@ -0,0 +1,32 @@
+package org.designpattern.singleton;
+
+import java.io.IOException;
+import java.util.Properties;
+
+enum AppConfig{
+    INSTANCE;
+
+    private final Properties properties;
+
+    AppConfig(){
+        properties = new Properties();
+
+        try{
+            properties.load(getClass().getResourceAsStream("/env.properties"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
+
+public class Enum {
+
+    public static void main(String[] args) throws IOException{
+        String url = AppConfig.INSTANCE.getProperty("db-url");
+        System.out.println("DB URL: " + url );
+    }
+}
